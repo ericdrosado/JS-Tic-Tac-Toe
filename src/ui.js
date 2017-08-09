0c8fc01fd2925688a310a8cb3a0d1999b7dcbeb2
Eric Rosado <rosadoericd@gmail.com>
2017-08-09 16:22:17 -0500

    Working, refactored, and tested story 55

--- src/ui.js
+++ src/ui.js
@@ -3,15 +3,25 @@ var $ = require('jquery');
 function UI() {
 }
 
-UI.prototype.spotClicked = function(e) {
-  var id = parseInt($(e.target).attr("id"));
-  console.log("SPOT CLICKED WITH ID: " + id);
-  return id;
-}
-
 UI.prototype.displayTurn = function(marker) {
   $("#turn-label").html(marker + "'s turn!");
-  return marker;
+}
+
+UI.prototype.onSpotClicked = function(callback) {
+  $(".spot").on("click", (e) => {
+    callback(e);
+  });
+}
+
+UI.prototype.spotClicked = function(e, playerMarker) {
+  var id = parseInt($(e.target).attr("id"));
+  UI.prototype.disableClick(e);
+  UI.prototype.displayMarker(id, playerMarker);
+  console.log("SPOT CLICKED WITH ID: " + id);
+}
+
+UI.prototype.disableClick = function(e){
+  $(e.target).addClass("disabled").off("click");
 }
 
 UI.prototype.displayMarker = function(id, playerMarker) {
