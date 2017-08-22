42c80dbb5667e745d683bc84c122082fa1db0182
Eric Rosado <rosadoericd@gmail.com>
2017-08-22 08:48:33 -0500

    Fixed gameBoard bug.

--- spec/PlayerVsPlayerGameSpec.js
+++ spec/PlayerVsPlayerGameSpec.js
@@ -1,6 +1,31 @@
-var MockUI = require('./mocks/MockUI');
+var $ = require('jquery');
+var GameBoard = require('../src/GameBoard');
 var PlayerVsPlayerGame = require('../src/games/PlayerVsPlayerGame');
+var UI = require('../src/ui');
+var WinConditions = require('../src/WinConditions');
 
 describe("PlayerVsPlayerGame", function() {
-  
+
+  beforeEach(function() {
+    gameBoard = new GameBoard();
+    ui = new UI();
+    winConditions = new WinConditions();
+    game = new PlayerVsPlayerGame(ui, gameBoard, winConditions);
+    jasmine.getFixtures().fixturesPath = '../partials/'
+    jasmine.getFixtures().load('game.html');
+  });
+
+  describe("#play", function(){
+    it("will display a marker after a location has been chosen", function() {
+      game.initializeGame();
+      $(".spot").attr({id: "0"}).trigger("click");
+      expect($("#0").text()).toEqual("X");
+    });
+    it("will display a marker after a location has been chosen", function() {
+      game.initializeGame();
+      $(".spot").attr({id: "1"}).trigger("click");
+      expect($("#1").text()).toEqual("X");
+    });
+  });
+
 });
